7292fdb182eeb7e958c9b55cc8099f4708bb6238
Jesse Sipari <Jesse.Sipari@student.lab.fi>
2024-04-19 18:56:02 +0300

    Initial commit

---
+++ Timber.cpp
@@ -0,0 +1,94 @@
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+
+// This is where our game starts from
+int main()
+{
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	// Create a texture to hold a graphic on the GPU
+	Texture textureBackground;
+
+	// Load a graphic into the texture
+	textureBackground.loadFromFile("graphics/background.png");
+
+	// Create a sprite
+	Sprite spriteBackground;
+
+	// Attach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+
+	// Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
+
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
+	// Prepare the bee
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+
+	// Is the bee currently moving?
+	bool beeActive = false;
+
+	// How fast can the bee fly
+	float beeSpeed = 0.0f;
+
+	// make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+
+	// Load 1 new texture
+	textureCloud.loadFromFile("graphics/cloud.png")
+
+	// 3 New sprites with the same texture
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	// Position the clouds on the left of the screen at different heights
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 250);
+	spriteCloud3.setPosition(0, 500);
+
+	while (window.isOpen())
+	{
+				// Handle the player input
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+						window.close();
+		}
+
+		/*
+		 ***************************
+		 Draw the scene
+		 ***************************
+		 */
+
+		// Clear everything from the last frame
+		window.clear();
+
+		// Draw our game scene here
+		window.draw(spriteBackground);
+
+		// Draw our game scene here
+		window.display();
+	}
+	return 0;
+}
